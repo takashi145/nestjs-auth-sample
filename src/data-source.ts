4d926395ab990d3c2d2c58b0137898b24cd6903b
takashi145 <t.shuhei0203@gmail.com>
2023-04-16 23:26:26 +0900

    update main.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
-import 'dotenv/config';
 import { User } from './entities/user.entity';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
